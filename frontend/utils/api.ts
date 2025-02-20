f0d553ce53ab3b6fbab1dbfee530463e7ab91959
avocadocodes <nikitakar82925@gmail.com>
2025-02-20 20:13:52 +0530

    db change

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/todos';
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/todos`;
+
 
 export const fetchTodos = async () => {
   const response = await axios.get(API_URL);
